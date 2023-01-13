aaed9acf0025ef0f5e8108bd1e64cbf4b3340e07
BrodyDubach <brodydubach@gmail.com>
2023-01-13 18:13:37 +0000

    First commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
 import { Link } from "react-router-dom";
+import { Context } from '../store/appContext';
 
 export const Navbar = () => {
+	const [favoriteItems, setFavoriteItems] = useState([]);
+	const {store, actions} = useContext(Context)
+
+	useEffect(() => {
+		console.log('favorites', store.favorites)
+		setFavoriteItems(store.favorites)
+	}, [store.favorites])
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		
+		<nav className="navbar navbar-black bg-black mb-0">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span style={{marginLeft: '4rem', color: 'gold'}} className="navbar-brand mb-0 h1"><img className="navLogo" src="https://www.freepnglogos.com/uploads/star-wars-logo-1.png" alt="StarWars logo" width="70" height="60"></img></span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div style={{marginRight: '4rem'}} class="dropdown">
+				<button class="btn btn-transparent dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+					<p class="favorites">Favorites</p>
+				</button>
+				<ul class="dropdown-menu">
+					{favoriteItems.length ? favoriteItems.map(item => {
+						return (
+							<li style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}} class="dropdown-item">{item.character ? item.character.name : item.planet ? item.planet.name : item.vehicle ? item.vehicle.name : null}<button onClick={() => {actions.removeFavorite(item)}}>X</button></li>
+						)
+					}) : <li style={{display: 'flex', justifyContent: 'center'}}>No favorites.</li>}
+				</ul>
 			</div>
 		</nav>
 	);
